e4585e2569923f0f7f5e811a187ad49709bf5197
Arief Yusron <ariefyusron98@gmail.com>
2018-11-26 13:49:38 +0700

    Belajar 1.3 Component

--- App.js
+++ App.js
@@ -3,6 +3,14 @@ import {View, Text} from 'react-native';
 
 export default class Index extends Component {
 
+  componentWillMount(){
+    console.log('Component Will Mount');
+  }
+
+  componentDidMount(){
+    console.log('Component Did Mount');
+  }
+
   render(){
     return (
       <View>
