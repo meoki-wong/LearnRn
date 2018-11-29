c93e89b185bf8bba62060f73803fde9aeb398dd9
Arief Yusron <ariefyusron98@gmail.com>
2018-11-30 03:09:45 +0700

    1.6 State

--- App.js
+++ App.js
@@ -3,23 +3,59 @@ import {View, Text, TouchableOpacity} from 'react-native';
 
 export default class Index extends Component {
 
+  // state = {
+  //   nama: 'Arief Yusron',
+  //   asal: 'Sumenep',
+  //   no: 0,
+  //   detik: 0,
+  // }
+
+  // ini sama aja. cuman kebanyakan orang pakek ini
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      nama: 'Arief Yusron',
+      asal: 'Sumenep',
+      no: 0,
+      detik: 0,      
+    };
+  }
+
+  componentDidMount(){
+    setInterval(()=>{
+      this.setState({
+        detik: this.state.detik + 1,
+      });
+    },1000);
+  }
+
   handleClick(i){
-    alert('Halo '+i.nama);
+    this.setState({
+      asal: i,
+    });
+  }
+
+  handleClickTwo(){
+    this.setState({
+      no: this.state.no+1,
+    });
   }
 
   render(){
 
-    const profil = {
-      nama: 'Arief Yusron',
-      asal: 'Sumenep'
-    }
-
     return (
       <View>
-        <Text>Please Click</Text>
-        <TouchableOpacity onPress={()=>this.handleClick(profil)}>
+        <Text>{this.state.nama}</Text>
+        <Text>{this.state.asal}</Text>
+        <TouchableOpacity onPress={()=>this.handleClick('Madiun')}>
           <Text>Click</Text>
         </TouchableOpacity>
+        <TouchableOpacity onPress={()=>this.handleClickTwo()}>
+          <Text>Tambah</Text>
+        </TouchableOpacity>
+        <Text>{this.state.no}</Text>
+        <Text>{this.state.detik} detik</Text>
       </View>
     )
   }
